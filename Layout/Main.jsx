ad098c6c11c6550ee929a9016bec126bd21f96e5
MostakemHossain <mostakememon123@gmail.com>
2023-05-14 20:12:16 +0600

    header and footer section added

--- Layout/Main.jsx
+++ Layout/Main.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../src/Pages/Shared/Footer/Footer";
+import Header from "../src/Pages/Shared/Header/Header";
 
 
 const Main = () => {
     return (
         <div>
+            <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
