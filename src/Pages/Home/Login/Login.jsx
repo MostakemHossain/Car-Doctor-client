81d68daf83324c9f2bfdbe06458d02dfced64a69
MostakemHossain <mostakememon123@gmail.com>
2023-05-15 19:57:01 +0600

    firebase setup

--- src/Pages/Home/Login/Login.jsx
+++ src/Pages/Home/Login/Login.jsx
@@ -2,32 +2,42 @@
 import login from '../../../assets/images/login/login.svg'
 
 const Login = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+    }
+
     return (
         <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row items-center gap-5">
-                <div className="text-center lg:text-left w-1/2">
+            <div className="hero-content flex-col lg:flex-row">
+                <div className="p-10 w-1/2">
+
                     <img src={login} alt="" />
                 </div>
-                <div className="card flex-shrink-0 w-1/2 max-w-sm shadow-2xl bg-base-100">
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
-                        </div>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
-                        </div>
+                        <h1 className="text-3xl text-center font-bold">Login now!</h1>
+                        <form onSubmit={handleSubmit}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+
+                                <input className="btn btn-primary" type="submit" value="login" />
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
